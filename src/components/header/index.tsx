68353fe3e70adfaaf9a726478fb579d29d56fc85
Shaw996 <shawliu996@gmail.com>
2024-10-21 08:50:50 +0800

    feat: 🚀 菜单

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -106,46 +106,20 @@ const Header = () => {
         </nav>
 
         {/* 图标按钮区域 */}
-        <div className="flex items-center space-x-4">
-          <IconButton onClick={toggleToolsMenu} icon={<Wrench />} />
+        <div className="flex items-center [&>*:not(:last-child)]:mr-4">
+          <IconButton className="md:hidden" onClick={toggleToolsMenu} icon={<Wrench />} />
           <IconButton
             onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
             icon={resolvedTheme === 'dark' ? <Sun /> : <Moon />}
           />
           <IconButton icon={<User />} />
-          <IconButton onClick={toggleMobileMenu} className="md:hidden" icon={<Menu />} />
+          <IconButton onClick={toggleMobileMenu} icon={<Menu />} />
         </div>
       </div>
 
       {/* 小屏幕工具菜单下拉显示，点击扳手图标时展示 */}
       {isToolsMenuOpen && (
-        <div className="md:hidden mt-4 bg-white dark:bg-gray-900 shadow-lg rounded-lg p-4 absolute right-0 w-full">
-          <Accordion type="single" collapsible className="w-full">
-            {toolsConfig.map((tool, index) =>
-              'items' in tool ? (
-                <AccordionItem key={index} value={tool.label}>
-                  <AccordionTrigger>{tool.label}</AccordionTrigger>
-                  <AccordionContent>
-                    {tool.items.map((item, i) => (
-                      <NavLink key={i} href={item.path}>
-                        {item.icon} {item.label}
-                      </NavLink>
-                    ))}
-                  </AccordionContent>
-                </AccordionItem>
-              ) : (
-                <NavLink key={index} href={tool.path}>
-                  {tool.label}
-                </NavLink>
-              ),
-            )}
-          </Accordion>
-        </div>
-      )}
-
-      {/* 小屏幕侧边栏菜单，使用Accordion组件 */}
-      {isMobileMenuOpen && (
-        <Drawer title="工具箱" emitClose={toggleMobileMenu}>
+        <Drawer title="工具箱" emitClose={toggleToolsMenu}>
           <Accordion type="single" collapsible className="w-full">
             {toolsConfig.map((tool, index) =>
               'items' in tool ? (
@@ -171,6 +145,65 @@ const Header = () => {
             )}
           </Accordion>
         </Drawer>
+      )}
+
+      {/* 菜单 */}
+      {isMobileMenuOpen && (
+        <>
+          {/* 大屏幕菜单 */}
+          <menu className="px-4 pb-4 pt-2 hidden border-t [&>*:not(:last-child)]:mr-16 md:flex">
+            {toolsConfig.map((tool, index) => {
+              if ('items' in tool) {
+                return (
+                  <ul key={index}>
+                    <li className="py-2 font-medium text-zinc-400">{tool.label}</li>
+                    {tool.items.map((item, i) => {
+                      return (
+                        <li key={i} className="py-1 text-sm">
+                          <NavLink href={item.path}>{item.label}</NavLink>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                );
+              }
+
+              return (
+                <li className="py-2 font-medium" key={index}>
+                  <NavLink href={tool.path}>{tool.label}</NavLink>
+                </li>
+              );
+            })}
+          </menu>
+
+          {/* 小屏幕侧边栏菜单，使用Accordion组件 */}
+          <Drawer className="md:hidden" title="菜单" mode="toRight" emitClose={toggleMobileMenu}>
+            <Accordion type="single" collapsible className="w-full">
+              {toolsConfig.map((tool, index) =>
+                'items' in tool ? (
+                  <AccordionItem key={index} value={tool.label}>
+                    <AccordionTrigger>{tool.label}</AccordionTrigger>
+                    <AccordionContent>
+                      {tool.items.map((item, i) => (
+                        <NavLink key={i} href={item.path}>
+                          <span className="py-2 flex flex-1 items-center font-medium transition-all hover:underline">
+                            &nbsp;&nbsp;{item.icon}&nbsp;&nbsp;{item.label}
+                          </span>
+                        </NavLink>
+                      ))}
+                    </AccordionContent>
+                  </AccordionItem>
+                ) : (
+                  <NavLink key={index} href={tool.path}>
+                    <span className="py-4 flex flex-1 items-center justify-between font-medium text-[hsl(var(--foreground))] transition-all border-b hover:underline">
+                      {tool.label}
+                    </span>
+                  </NavLink>
+                ),
+              )}
+            </Accordion>
+          </Drawer>
+        </>
       )}
     </header>
   );
@@ -221,7 +254,13 @@ const DropdownMenuItem = ({
 );
 
 // 菜单下拉组件
-const DropdownMenu = ({ label, children }: { label: string; children: React.ReactNode }) => (
+const DropdownMenu = ({
+  label,
+  children,
+}: {
+  label: string | React.ReactNode;
+  children: React.ReactNode;
+}) => (
   <div className="relative group">
     <div className="group flex items-center text-gray-800 dark:text-gray-100 hover:text-red-500 transition duration-300">
       {label}
--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -1,20 +1,74 @@
 import { useEffect, useRef, useState } from 'react';
+import { cva } from 'class-variance-authority';
 
 import Mask from './mask';
 
 import { cn } from '@/utils';
 
+const drawerVariants = cva(
+  'DrawerWrapper flex flex-col w-full px-4 py-4 absolute transition-transform duration-150 bg-white dark:bg-gray-900',
+  {
+    variants: {
+      mode: {
+        toBottom: 'min-h-[30%] max-h-[80%] top-0 -translate-y-full rounded-b-2xl',
+        toTop: 'min-h-[30%] max-h-[80%] bottom-0 translate-y-full rounded-t-2xl',
+        toLeft: 'w-3/4 h-full right-0 translate-x-full rounded-l-xl',
+        toRight: 'w-3/4 h-full left-0 -translate-x-full rounded-r-xl',
+      },
+    },
+    defaultVariants: {
+      mode: 'toTop',
+    },
+  },
+);
+
 const Drawer = ({
+  className = '',
   children,
+  mode,
   title,
   emitClose,
 }: {
   children: React.ReactNode;
+  className?: string;
+  mode?: 'toBottom' | 'toTop' | 'toLeft' | 'toRight';
   title?: string;
   emitClose: () => void;
 }) => {
   const closeTimeout = useRef<NodeJS.Timeout | null>(null);
   const [isEnter, setIsEnter] = useState(false);
+
+  let titleClassName = '';
+
+  if (title) {
+    switch (mode) {
+      case 'toLeft':
+      case 'toRight':
+        titleClassName = 'text-left';
+        break;
+      case 'toTop':
+      case 'toBottom':
+      default:
+        titleClassName = 'text-center';
+        break;
+    }
+  }
+
+  let transitionClassName = '';
+
+  if (isEnter) {
+    switch (mode) {
+      case 'toLeft':
+      case 'toRight':
+        transitionClassName = 'translate-x-0';
+        break;
+      case 'toBottom':
+      case 'toTop':
+      default:
+        transitionClassName = 'translate-y-0';
+        break;
+    }
+  }
 
   const clickMask = () => {
     if (closeTimeout.current) {
@@ -39,19 +93,22 @@ const Drawer = ({
   }, []);
 
   return (
-    <Mask emitClick={clickMask}>
-      {/* Drawer组件内容区域 */}
-      <div
-        className={cn(
-          'DrawerWrapper flex flex-col w-full min-h-[30%] max-h-[80%] px-4 py-4 absolute bottom-0 translate-y-full transition-transform duration-150 bg-white rounded-t-2xl',
-          isEnter ? 'translate-y-0' : '',
-        )}
-        onClick={(e) => e.stopPropagation()}
-      >
-        {title && <h3 className="flex-none text-center text-lg font-bold">{title}</h3>}
-        <div className="DrawerScrollable flex-1 overflow-y-auto">{children}</div>
-      </div>
-    </Mask>
+    <div className={className}>
+      <Mask emitClick={clickMask}>
+        {/* Drawer组件内容区域 */}
+        <div
+          className={cn(drawerVariants({ mode }), transitionClassName)}
+          onClick={(e) => e.stopPropagation()}
+        >
+          {title && (
+            <h3 className={cn('mb-2 flex-none text-lg font-bold', titleClassName)}>{title}</h3>
+          )}
+          <div className="DrawerScrollable flex-1 overflow-x-hidden overflow-y-auto">
+            {children}
+          </div>
+        </div>
+      </Mask>
+    </div>
   );
 };
 
